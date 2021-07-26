d7b75996ce0f8f1689a5dfca3ab375942f8fb5e1
Camila <amandaforero@outlook.com>
2021-07-26 08:33:28 -0500

    Manejo del DOM en main.js, se colocan div con las imagenes y nombres de la data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import data from './data/lol/lol.js';
 
 
+let values=Object.values(data.data)
+Object.entries(values).forEach(([key, value]) =>{
+    let containerBox=`<div class="champion_container">
+<img class="champion_image" src="${value.splash}">
+<p class="champion_name">${value.name}</p>
+</div>
+</div>`
+const champions=document.querySelector(".champions");
+champions.append(document.createRange().createContextualFragment(containerBox))
 
-// console.log(example, data);
+});
+
+
+
+
+
+
+
+
+
+// for(let index=0; index<values.length; index++){
+//     if (index
+//     console.log(value[index].id)
+// }
+
